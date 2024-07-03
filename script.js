e9df329dd5de49b6ea6d337fd1c0ed9b25b6cad0
noothangimounika <noothangimounika@gmail.com>
2024-07-03 13:28:35 +0530

    Add Blog-page and properties-page

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
-var onclick=document.getElementById('onclick');
-function realtiFye(){
-    onclick.style.display='block';
-}
+// var search=document.createElement('div');
+// search.style.display='flex';
+// var selection1= document.createElement('div');
+// var imageicon=document.createElement('img');
+// imageicon.src='images/locationicon.jpg';
+// imageicon.style.width='30px';
+// imageicon.style.height='30px';
+// selection1.appendChild(imageicon);
+
+// var subselect =document.createElement('select')
+// var options=["Location" , "Washinhton.dc" ,"NewYork" , "arkansas"];
+// options.forEach(optionitems => {
+// var option=document.createElement('option');
+// option.value=optionitems;
+// option.textContent= optionitems;
+// subselect.appendChild(option);
+
+// });
+// selection1.appendChild(subselect);
+
+
+// var selection2= document.createElement('div');
+// var imageicon1=document.createElement('img');
+
+// imageicon1.src='images/apartmenticon.jpg';
+// imageicon1.style.width='50px';
+// imageicon1.style.height='50px';
+
+// selection2.appendChild(imageicon1);
+// var subselect1 =document.createElement('select')
+// var options1=["Apartments" , "Flat" ,"Plots" ];
+// options1.forEach(optionitems1 => {
+// var option1=document.createElement('option');
+// option1.value=optionitems1;
+// option1.textContent= optionitems1;
+// subselect1.appendChild(option1);
+
+// });
+// selection2.appendChild(subselect1);
+
+
+// var selection3= document.createElement('div');
+// var imageicon2=document.createElement('img');
+// imageicon2.src='images/dollaricon.png';
+// imageicon2.style.width='50px';
+// imageicon2.style.height='50px';
+
+// selection3.appendChild(imageicon2);
+
+// var subselect2 =document.createElement('select')
+// var options2=["$50k - $125k" , "$126k - $300k" ,"$301k - $500k" ];
+// options2.forEach(optionitems2 => {
+// var option2=document.createElement('option');
+// option2.value=optionitems2;
+// option2.textContent= optionitems2;
+// subselect2.appendChild(option2);
+
+// });
+// selection3.appendChild(subselect2);
+// var btn1=document.createElement('button');
+// btn1.textContent='search';
+// btn1.style.backgroundColor='orange';
+// btn1.style.borderRadius='15px';
+// btn1.style.padding='5px';
+// btn1.style.width='150px';
+// btn1.style.height='30px';
+
+// search.appendChild(selection1);
+// search.appendChild(selection2);
+// search.appendChild(selection3);
+// search.appendChild(btn1); //position of appendchilds
+// Rajesh.appendChild(search);
